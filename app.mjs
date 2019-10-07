daef2aa2106362b40ae9b601e3a24dd820fd8343
Alex Weisberger <alex.weisberger@viewthespace.com>
2019-10-06 22:19:08 -0400

    Use server obj to add events to db

--- app.mjs
+++ app.mjs
@@ -1,8 +1,20 @@
 import express from 'express';
-import commands, { makeServer } from 'calendar-behavior/mod.babel.mjs';
+import bodyParser from 'body-parser';
+import { makeServer, makeSqlRepository } from 'calendar-behavior/mod.babel.mjs';
+import { postgresDatastore } from './db.mjs';
 const app = express()
 const port = 3002
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(bodyParser.json());
+
+let repository = makeSqlRepository(postgresDatastore);
+let server = makeServer(repository);
+
+app.post('/events', async (req, res) => {
+  let { name, date } = req.body;
+  server.addEvent(name, date)
+    .then(() => res.send('ok'))
+    .catch((err) => res.send(err.message));
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ db.mjs
@@ -0,0 +1,15 @@
+import { makeSqlRepository } from 'calendar-behavior/mod.babel.mjs';
+import pgp from 'pg-promise';
+
+let db = pgp()('postgres://alexweisberger:@localhost:5432/calendar');
+
+let postgresDatastore = {
+  async setup(query) {
+    await db.none(query);
+  },
+  execute(query, ...args) {
+    return db.any(query, args);
+  }
+};
+
+export { postgresDatastore };
